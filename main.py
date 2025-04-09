6b19bfafa079ac216c213a31c2e1d53b84397e44
NikitaKrinkin <nikita@krinkin.com>
2025-04-10 01:32:34 +0300

    blop

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/year-1945-1968/')
 def index():
-    return render_template('home.html')
+    return render_template('Y_1945_1968.html')
 
 
 if __name__ == '__main__':
