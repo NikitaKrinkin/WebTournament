c6973a8b93097b0007851fab6f4b068125ed417f
NikitaKrinkin <nikita@krinkin.com>
2025-04-11 22:14:50 +0300

    Main fixes

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/year-1945-1968/')
+@app.route('/')
 def index():
+    return render_template('home.html')
+
+
+@app.route('/year-1945-1968/')
+def year():
     return render_template('Y_1945_1968.html')
 
 
